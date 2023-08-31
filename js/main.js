fce9a5771c0d7cce26561799b486051c047552c7
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-08-31 14:47:53 +0200

    add: create list with Vue.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 const { createApp } = Vue;
 
+const toDoList = [
+  {
+    text: "Fare una lista",
+    done: false,
+  },
+  {
+    text: "Aggiungere una nuova attività",
+    done: false,
+  },
+  {
+    text: "Eliminare una attività",
+    done: false,
+  },
+];
+
 createApp({
   data() {
     return {
       message: "Hello World",
+      toDoList,
     };
+  },
+
+  created() {
+    console.log(this.toDoList);
   },
 }).mount("#app");
